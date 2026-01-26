2c4a50ba7aacf534b5e3dac89dbb1a79040ff43d
SunnySkai <sunnyskaies@gmail.com>
2026-01-26 01:28:13 -0800

    [fix] corner issue

--- StitchVision/Views/PlaceholderViews.swift
+++ StitchVision/Views/PlaceholderViews.swift
@@ -2716,12 +2716,6 @@ struct CelebratingMascotView: View {
     }
 }
 
-extension RoundedRectangle {
-    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-        clipShape(RoundedCorner(radius: radius, corners: corners))
-    }
-}
-
 struct RoundedCorner: Shape {
     var radius: CGFloat = .infinity
     var corners: UIRectCorner = .allCorners
