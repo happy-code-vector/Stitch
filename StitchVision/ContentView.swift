d90e6a0b133fca7501df970f1abd171bae61f9aa
SunnySkai <sunnyskaies@gmail.com>
2026-01-30 12:02:04 -0500

    [fix] issues

--- StitchVision/ContentView.swift
+++ StitchVision/ContentView.swift
@@ -4,71 +4,69 @@ struct ContentView: View {
     @StateObject private var appState = AppState()
     
     var body: some View {
-        NavigationStack {
-            Group {
-                switch appState.currentScreen {
-                case .splash:
-                    SplashView()
-                case .craft:
-                    CraftSelectionView()
-                case .skill:
-                    SkillLevelView()
-                case .struggle:
-                    StruggleView()
-                case .statsProblem:
-                    StatsProblemView()
-                case .habit:
-                    HabitFrequencyView()
-                case .goal:
-                    GoalSettingView()
-                case .statsSolution:
-                    StatsSolutionView()
-                case .loading:
-                    LoadingView()
-                case .result:
-                    ResultView()
-                case .howItWorks:
-                    HowItWorksView()
-                case .stats:
-                    StatsView()
-                case .cameraPermissions:
-                    CameraPermissionsView()
-                case .calibration:
-                    CalibrationView()
-                case .subscription:
-                    SubscriptionView()
-                case .downsell:
-                    DownsellView()
-                case .permissions:
-                    PermissionsView()
-                case .freeTierWelcome:
-                    FreeTierWelcomeView()
-                case .dashboard:
-                    DashboardView()
-                case .workMode:
-                    WorkModeView()
-                case .sessionSummary:
-                    SessionSummaryView(
-                        rowsKnit: appState.sessionData.rowsKnit,
-                        timeSpent: appState.sessionData.timeSpent
-                    )
-                case .settings:
-                    SettingsView()
-                case .patternVerification:
-                    PatternVerificationView()
-                case .projectSetup:
-                    ProjectSetupView()
-                case .projectDetail:
-                    ProjectDetailView()
-                case .patternUpload:
-                    PatternUploadView()
-                case .help:
-                    HelpSupportView()
-                case .notifications:
-                    NotificationsView()
-                case .profile:
-                    ProfileEditorView()
-                }
+        Group {
+            switch appState.currentScreen {
+            case .splash:
+                SplashView()
+            case .craft:
+                CraftSelectionView()
+            case .skill:
+                SkillLevelView()
+            case .struggle:
+                StruggleView()
+            case .statsProblem:
+                StatsProblemView()
+            case .habit:
+                HabitFrequencyView()
+            case .goal:
+                GoalSettingView()
+            case .statsSolution:
+                StatsSolutionView()
+            case .loading:
+                LoadingView()
+            case .result:
+                ResultView()
+            case .howItWorks:
+                HowItWorksView()
+            case .stats:
+                StatsView()
+            case .cameraPermissions:
+                CameraPermissionsView()
+            case .calibration:
+                CalibrationView()
+            case .subscription:
+                SubscriptionView()
+            case .downsell:
+                DownsellView()
+            case .permissions:
+                PermissionsView()
+            case .freeTierWelcome:
+                FreeTierWelcomeView()
+            case .dashboard:
+                DashboardView()
+            case .workMode:
+                WorkModeView()
+            case .sessionSummary:
+                SessionSummaryView(
+                    rowsKnit: appState.sessionData.rowsKnit,
+                    timeSpent: appState.sessionData.timeSpent
+                )
+            case .settings:
+                SettingsView()
+            case .patternVerification:
+                PatternVerificationView()
+            case .projectSetup:
+                ProjectSetupView()
+            case .projectDetail:
+                ProjectDetailView()
+            case .patternUpload:
+                PatternUploadView()
+            case .help:
+                HelpSupportView()
+            case .notifications:
+                NotificationsView()
+            case .profile:
+                ProfileEditorView()
             }
         }
         .environmentObject(appState)
